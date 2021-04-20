b473e86e7ab9cca40cb4f367d404faa6af4ed043
Volker Shmuley <imnaishvili@gmail.com>
2021-04-20 20:46:42 +0300

    Go Live

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-var fs = require("fs");
-var text = fs.readFileSync("./proverbs.txt").toString('utf-8');
-var textByLine = text.split("\n")
+// NodeJs method to turn txt file into array
+// var fs = require("fs");
+// var text = fs.readFileSync("./proverbs.txt").toString('utf-8');
+// var textByLine = text.split("\n")
 
-console.log(textByLine[78]);
+// This app only works on live server. You get Access errors without it.
+
+import { proverbList } from './proverbs.js';
+
+const buttonElement = document.getElementById('generateButton');
+const pElement = document.getElementById('proverb');
+
+function generateCookie() {
+    var randomIndex = Math.floor(Math.random() * proverbList.length);
+    return proverbList[randomIndex];
+}
+
+function insertProverb() {
+    var proverb = generateCookie();
+    pElement.innerHTML = proverb;
+}
+
+buttonElement.addEventListener('click', insertProverb);
